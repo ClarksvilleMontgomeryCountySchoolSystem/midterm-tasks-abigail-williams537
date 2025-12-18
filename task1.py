9991a6dc94fc09e98cf917199b11a91941b4796a
abigail-williams537 <abigail.williams537@students.cmcss.net>
2025-12-18 08:38:57 -0600

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,1 +1,23 @@
 
+slices = party_pizza_mini + large + medium
+print(f"Total Number of Slices: {slices}")
+
+people += 1
+share = slices // people
+leftover = slices % people
+print(f"Each person gets: {share}")
+print(f"Leftover slices: {leftover}")
+
+people += 2 #Eric and Brandon are coming too.
+share = slices // people
+leftovers = slices % people
+print(f"Each person gets: {share}")
+print(f"Leftover slices: {leftovers}") 
+
+
+slices += party_pizza_mini
+share = slices // people
+leftovers = slices % people
+print(f"Each person gets: {share}")
+print(f"Leftover slices: {leftovers}")
+print("...for Mr. Hollow Leg")
